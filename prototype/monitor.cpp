ed5c82731fee8ac257bd0bcdaa1cbb41ffc32240
Ilya Razenshteyn <ilyaraz@ilyaraz-debian-vmware.localdomain>
2013-04-20 11:27:22 -0400

    full stats

--- prototype/monitor.cpp
+++ prototype/monitor.cpp
@@ -7,6 +7,10 @@
 
 void printStatistics(const GetStatisticsReply &statistics) {
     std::cout << "insertions: " << statistics.numInsertions << std::endl;
+    std::cout << "updates: " << statistics.numUpdates << std::endl;
+    std::cout << "retrieves: " << statistics.numGoodGets << std::endl;
+    std::cout << "misses: " << statistics.numFailedGets << std::endl;
+    std::cout << std::endl;
 }
 
 int main(int argc, char **argv) {
