274e58433f5f61400a8606b0463c40f3cabcbd81
Ilya Razenshteyn <ilyaraz@ilyaraz-debian-vmware.localdomain>
2013-04-20 21:49:51 -0400

    counting ops

--- prototype/monitor.cpp
+++ prototype/monitor.cpp
@@ -5,11 +5,12 @@
 #include <unistd.h>
 #include <iostream>
 
-void printStatistics(const GetStatisticsReply &statistics) {
-    std::cout << "insertions: " << statistics.numInsertions << std::endl;
-    std::cout << "updates: " << statistics.numUpdates << std::endl;
-    std::cout << "retrieves: " << statistics.numGoodGets << std::endl;
-    std::cout << "misses: " << statistics.numFailedGets << std::endl;
+long long getOps(const GetStatisticsReply &s) {
+    return s.numInsertions + s.numUpdates + s.numGoodGets + s.numFailedGets;
+}
+
+void printStatistics(const GetStatisticsReply &statistics, const GetStatisticsReply &last) {
+    std::cout << "ops: " << getOps(statistics) - getOps(last) << std::endl;
     std::cout << "data size: " << statistics.dataSize << std::endl;
     std::cout << "hash tables sizes: " << statistics.memoryOverhead << std::endl;
     std::cout << "evictions: " << statistics.numEvictions << std::endl;
@@ -19,11 +20,13 @@ void printStatistics(const GetStatisticsReply &statistics) {
 int main(int argc, char **argv) {
     std::string server = argv[1];
     int port = atoi(argv[2]);
+    GetStatisticsReply last;
     for (;;) {
         ServerConnection<ViewServiceClient> connection(server, port);
         GetStatisticsReply statistics;
         connection.getClient()->getStatistics(statistics);
-        printStatistics(statistics);
+        printStatistics(statistics, last);
+        last = statistics;
         sleep(1);
     }
     return 0;
