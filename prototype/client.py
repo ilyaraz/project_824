b63b57424c1017d094f5a14c15e3ae7e407a1a2c
Gautam Kamath <g@csail.mit.edu>
2013-04-29 00:40:55 -0400

    Mostly working client

--- prototype/client.py
+++ prototype/client.py
@@ -32,17 +32,19 @@ class CacheClient:
     client = ViewService.Client(connection.getProtocol())
     self.servers = client.getView().servers
     connection.close()
-    print str(len(self.servers)) + " servers in charge\n"
+    print str(len(self.servers)) + " servers in charge"
 
   def get(self, key):
     try:
       serverID = self.getServerID(key)
       connection = ServerConnection(self.servers[serverID].server, self.servers[serverID].port)
-      client = KVStorageClient.Client(connection.getProtocol())
+      client = KVStorage.Client(connection.getProtocol())
       reply = client.get(GetArgs(key))
       connection.close()
       if (reply.status == Status.OK):
+        print "Got " + key + " " + reply.value
         return reply.value
+      print "Got " + key + " Nothing"
       return ""
     except: 
       raise Exception('Get failed')
@@ -51,17 +53,17 @@ class CacheClient:
     try:
       serverID = self.getServerID(key)
       connection = ServerConnection(self.servers[serverID].server, self.servers[serverID].port)
-      client = KVStorageClient.Client(connection.getProtocol())
+      client = KVStorage.Client(connection.getProtocol())
       reply = client.put(PutArgs(key, val))
       connection.close()
-      print "Put " + key + " " + value
+      print "Put " + key + " " + val
     except: 
       raise Exception('Put failed')
 
   def getServerID(self, key):
     result = 0
     for c in key:
-      result ^= int(c)
+      result ^= ord(c)
       result += (result << 1) + (result << 4) + (result << 7) + (result << 8) + (result << 24)
     return result % len(self.servers)
 
@@ -77,10 +79,10 @@ def main(args):
   numFailedGets = 0
 
   while True:
-    key = str(random.getrandbits(32))
+    key = str(random.getrandbits(4))
     cacheClient.getServerID(key)
     if (bool(random.getrandbits(1))):
-      value = str(random.getrandbits(64))
+      value = str(random.getrandbits(10))
       try:
         cacheClient.put(key, value)
       except:
@@ -90,6 +92,6 @@ def main(args):
         cacheClient.get(key)
       except:
         numFailedGets += 1
-
+        
 if __name__ == "__main__":
   main(sys.argv)
