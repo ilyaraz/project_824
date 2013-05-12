204be4c9d3a8788fec189ec8f3fdc0613772fa0a
Ilya Razenshteyn <ilyaraz@gmail.com>
2013-05-12 17:18:23 -0400

    time measurements

--- SimpleBlog/src/StressTest.java
+++ SimpleBlog/src/StressTest.java
@@ -1,30 +1,26 @@
-import java.io.File;
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.sql.Time;
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- * Created by IntelliJ IDEA.
- * User: ilyaraz
- * Date: 5/12/13
- * Time: 4:25 PM
- * To change this template use File | Settings | File Templates.
- */
 public class StressTest {
-    public static void main(String[] args) throws MalformedURLException, IOException {
+    public static void main(String[] args) throws IOException {
         Random random = new Random();
+        long t1 = System.currentTimeMillis();
+        int counter = 0;
         for (;;) {
             int userID = random.nextInt();
             URL url = new URL("http://127.0.0.1:8000/"+userID);
-            System.out.println(url);
             HttpURLConnection connection = (HttpURLConnection)url.openConnection();
             connection.connect();
-            System.out.println(new Scanner(connection.getInputStream()).useDelimiter("\\A").next().length());
+            long size = new Scanner(connection.getInputStream()).useDelimiter("\\A").next().length();
             connection.disconnect();
+            counter++;
+            if (counter % 1000 == 0) {
+                long t2 = System.currentTimeMillis();
+                System.out.println(counter * 1000 / (t2 - t1) + " " + size);
+            }
         }
     }
 }
